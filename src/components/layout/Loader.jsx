1fbf7a5d8d86a3a867579c37b708e34d9c6ab908
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-13 23:13:21 +0100

    Creating the Loader component

---
+++ src/components/layout/Loader.jsx
@@ -0,0 +1,16 @@
+import loader from '../../assets/loader.svg'
+
+const Spinner = () => {
+	return (
+		<div className='w-100 mt-20'>
+			<img
+				className='text-center mx-auto'
+				src={loader}
+				alt='loader'
+				style={{ width: '350px' }}
+			/>
+		</div>
+	)
+}
+
+export default Spinner
