53693298e17105b6911c6d18afec5901205927c3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-13 19:39:53 +0100

    Adding the Routes, Pages and the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+
 function App() {
 	return (
-		<>
-			<h1 className='text-3xl '>Hello world!</h1>
-		</>
+		<Router>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+				<main className='container mx-auto px-3 pb-12'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/notfound' element={<NotFound />} />
+						<Route path='/*' element={<NotFound />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
 	)
 }
 
