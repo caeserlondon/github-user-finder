efa88bc648794f331b0fd47037d1f8bcab24fad3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-13 23:06:24 +0100

    Creating the single user component

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+const UserItem = ({ user: { login, avatar_url } }) => {
+	return (
+		<div className='card drop-shadow-md compact side bg-base-100'>
+			<div className='flex-row items-center space-x-4 card-body'>
+				<div>
+					<div className='avatar'>
+						<div className='rounded-full shadow w-14 h-14'>
+							<img src={avatar_url} alt={login} />
+						</div>
+					</div>
+				</div>
+				<div>
+					<h2 className='card-title'>{login}</h2>
+					<Link
+						to={`/users/${login}`}
+						className='text-base-content text-opacity-40'
+					>
+						Visit Profile
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
+}
+
+export default UserItem
