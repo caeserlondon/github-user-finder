ecbd8729322465419ed5cb77f79a4c05d20c28a0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-13 23:11:21 +0100

    Configuring the Github Context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,30 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import Loader from '../layout/Loader'
+import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContex'
 
 const UserResults = () => {
-	const [users, setUsers] = useState([])
-	const [loading, setLoading] = useState(true)
+	const { users, loading, fetchUsers } = useContext(GithubContext)
 
 	useEffect(() => {
 		fetchUsers()
 	}, [])
 
-	const fetchUsers = async () => {
-		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-			headers: {
-				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-			},
-		})
-		const data = await response.json()
-		setUsers(data)
-		setLoading(false)
-	}
-
 	if (!loading) {
 		return (
 			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
 				{users.map((user) => (
-					<h3>{user.login}</h3>
+					<UserItem key={user.id} user={user} />
 				))}
 			</div>
 		)
