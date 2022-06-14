26a2bce467995f8777269bf767e98da4fd853d64
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-14 13:57:00 +0100

    Updating the style

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
-		<nav className='navbar mb-12 drop-shadow-md bg-neutral text-nutral-content'>
+		<nav className='navbar mb-14 drop-shadow-md bg-neutral text-nutral-content'>
 			<div className='container mx-auto'>
 				<div className='flex-none px-2 mx-2'>
 					<FaGithub className='inline pr-2 text-3xl' />
-					<Link to='/' className='text-lg font-bold align-middle'>
+					<Link to='/' className='text-2xl font-bold align-middle'>
 						Github User Finder
 					</Link>
 				</div>
