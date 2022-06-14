d020d2bca650e255b80c173da1cb51112135f862
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-14 21:17:15 +0100

    Adding the set loading action

--- src/context/github/GithubContex.js
+++ src/context/github/GithubContex.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
-		loading: true,
+		loading: false,
 	}
 
 	const [state, dispatch] = useReducer(githubReducer, initialState)
 
+	// for testing purposes
 	const fetchUsers = async () => {
+		setLoading()
 		const response = await fetch(`${GITHUB_URL}/users`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`,
@@ -27,6 +29,12 @@ export const GithubProvider = ({ children }) => {
 			payload: data,
 		})
 	}
+
+	const setLoading = () =>
+		dispatch({
+			type: 'SET_LOADING',
+		})
+
 	return (
 		<GithubContext.Provider
 			value={{
