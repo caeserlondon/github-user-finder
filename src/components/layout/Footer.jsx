beb2460bd6d3207290b0b4ed1e8b6bbf1212a2c0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-14 13:55:47 +0100

    Updating the API link

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,10 +6,16 @@ const Footer = () => {
 
 	return (
 		<footer className='footer flex p-10 text-nutral-content bg-neutral footer-center'>
+			<a
+				href='https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<img src={cat} alt='octocat' style={{ width: '60px' }} />
+			</a>
 			<p className='flex-1'>
 				Copyright &copy; {footerYear} All rights reserved
 			</p>
-			<img src={cat} alt='octocat' style={{ width: '60px' }} />
 		</footer>
 	)
 }
