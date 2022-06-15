62af6e8084719147236ec58d428464bf1c077bba
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-15 08:52:16 +0100

    Creating Alert context

---
+++ src/context/alerts/AlertContext.js
@@ -0,0 +1,27 @@
+import { createContext, useReducer } from 'react'
+import alertReducer from './AlertReducer'
+
+const AlertContext = createContext()
+
+export const AlertProvider = ({ children }) => {
+	const initialState = null
+
+	const [state, dispatch] = useReducer(alertReducer, initialState)
+
+	// SET AN ALERT
+	const setAlert = (message, type) => {
+		dispatch({
+			type: 'SET_ALERT',
+			payload: { message, type },
+		})
+		setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3500)
+	}
+
+	return (
+		<AlertContext.Provider value={{ alert: state, setAlert }}>
+			{children}
+		</AlertContext.Provider>
+	)
+}
+
+export default AlertContext
