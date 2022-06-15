0ad0aa6c9074c920e7db30454401c38e7653c4de
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-15 23:20:39 +0100

    Adding the cats to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,47 @@
+import aristo from '../assets/aristo.png'
+import cam from '../assets/cam.png'
+import cold from '../assets/cold.png'
+import gangham from '../assets/gangham.png'
+
+import search from '../assets/search.png'
+import walter from '../assets/walter.png'
+import what from '../assets/what.png'
+import wait from '../assets/wait.png'
+
 const About = () => {
 	return (
 		<div>
 			<h1 className='text-3xl mb-4 font-serif'>Github User Finder</h1>
 			<p className='text-xl mb-4 font-serif'>Search and find Github users</p>
 			<p className='text-base mb-4 font-serif'>Version: 1.0.0</p>
+			<div className='flex'>
+				<img
+					src={gangham}
+					alt='cat'
+					style={{ width: '120px', margin: 'auto' }}
+				/>
+				<img
+					src={aristo}
+					alt='cat'
+					style={{ width: '120px', margin: 'auto' }}
+				/>
+				<img src={cold} alt='cat' style={{ width: '120px', margin: 'auto' }} />
+
+				<img
+					src={walter}
+					alt='cat'
+					style={{ width: '120px', margin: 'auto' }}
+				/>
+				<img
+					src={search}
+					alt='cat'
+					style={{ width: '120px', margin: 'auto' }}
+				/>
+				<img src={wait} alt='cat' style={{ width: '120px', margin: 'auto' }} />
+				<img src={what} alt='cat' style={{ width: '120px', margin: 'auto' }} />
+
+				<img src={cam} alt='cat' style={{ width: '120px', margin: 'auto' }} />
+			</div>
 		</div>
 	)
 }
