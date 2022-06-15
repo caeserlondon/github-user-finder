66e4a2146df60019cf40304527ee53c73d3af75a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-15 14:46:35 +0100

    Fixing a typo

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -15,7 +15,7 @@ const UserItem = ({ user: { login, avatar_url } }) => {
 				<div>
 					<h2 className='card-title'>{login}</h2>
 					<Link
-						to={`/users/${login}`}
+						to={`/user/${login}`}
 						className='text-base-content text-opacity-40'
 					>
 						Visit Profile
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
 	return (
 		<div>
 			<h1 className='text-3xl mb-4 font-serif'>Github User Finder</h1>
-			<p className='text-xl mb-4 font-serif'>Search and fing Github users</p>
+			<p className='text-xl mb-4 font-serif'>Search and find Github users</p>
 			<p className='text-base mb-4 font-serif'>Version: 1.0.0</p>
 		</div>
 	)
