528837e599a42efabba2f29481c081a910a3157c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-10-25 09:47:16 +0100

    Adding the semicolons

--- src/context/alerts/AlertContext.js
+++ src/context/alerts/AlertContext.js
@@ -1,27 +1,27 @@
-import { createContext, useReducer } from 'react'
-import alertReducer from './AlertReducer'
+import { createContext, useReducer } from 'react';
+import alertReducer from './AlertReducer';
 
-const AlertContext = createContext()
+const AlertContext = createContext();
 
 export const AlertProvider = ({ children }) => {
-	const initialState = null
+	const initialState = null;
 
-	const [state, dispatch] = useReducer(alertReducer, initialState)
+	const [state, dispatch] = useReducer(alertReducer, initialState);
 
 	// SET AN ALERT
 	const setAlert = (message, type) => {
 		dispatch({
 			type: 'SET_ALERT',
 			payload: { message, type },
-		})
-		setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 5000)
-	}
+		});
+		setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 5000);
+	};
 
 	return (
 		<AlertContext.Provider value={{ alert: state, setAlert }}>
 			{children}
 		</AlertContext.Provider>
-	)
-}
+	);
+};
 
-export default AlertContext
+export default AlertContext;
